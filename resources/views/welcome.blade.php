76537f8cadcb37362e5e0c7949f941ad5fa9563e
yang-bendi <356550896@qq.com>
2017-09-29 06:16:05 +0000

    修改主页内容

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    杨国光的个人网站
                 </div>
 
                 <div class="links">
